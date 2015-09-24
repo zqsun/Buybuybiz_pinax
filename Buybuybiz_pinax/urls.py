80cfba721ab7ea83538b11c830158259816aec2e
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-09-24 11:17:51 -0700

    Add team page

--- Buybuybiz_pinax/urls.py
+++ Buybuybiz_pinax/urls.py
@@ -9,6 +9,7 @@ import userprofile.views
 urlpatterns = patterns(
     "",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^team/$", TemplateView.as_view(template_name="teampage.html"), name="team"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/signup/$", userprofile.views.SignupView.as_view(), name="account_signup"),
     url(r"^account/", include("account.urls")),
